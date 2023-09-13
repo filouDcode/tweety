5ed835e310b767700711d7f1379fa3981384b314
filouDcode <philippe-bouchard@hotmail.com>
2023-09-14 00:24:47 +0200

    fixed commit

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const index = require('./routes');
 const port = process.env.PORT || 3000; // soit le PORT est défini, soit on lui attribu la valeur 3000.
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'pug');
+app.set('view engine', 'pug');
 
-app.use(morgan, 'short');
+app.use(morgan('short'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(index);
 
-app.listen(PORT);
+app.listen(port);
