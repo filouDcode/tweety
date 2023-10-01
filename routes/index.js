21750b0d9e1c65e6092f54e894bbc89a488345c8
filouDcode <philippe-bouchard@hotmail.com>
2023-10-01 23:50:01 +0200

    topbar with logo & nav

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
-const router = require('express').Router();
+const express = require('express'); // Importe le module Express.js
+const router = express.Router(); // Crée un objet routeur Express et le stocke dans la variable "router"
+
+// const router = require('express').Router();
 
 router.get('/', (req, res) => {
 	res.render('home');
